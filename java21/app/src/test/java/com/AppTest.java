9d01a66335919c7bd183849855d576532548e52f
mshiray <10815743+mshiray@users.noreply.github.com>
2025-03-19 22:44:24 +0530

    initial commit

--- java21/app/src/test/java/com/AppTest.java
+++ java21/app/src/test/java/com/AppTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "Hello World!");
